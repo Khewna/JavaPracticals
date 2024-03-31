c6f03d4b5548cd051489f889dd7fede0c02a3da0
Khewna <165148322+Khewna@users.noreply.github.com>
2024-03-31 17:09:12 +0530

    Create Practical4.java

---
+++ Practical4.java
@@ -0,0 +1,33 @@
+package Practicals;
+
+import java.util.*;
+
+public class Practical4 {
+	
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter first input value:");
+		int a = sc.nextInt();
+		System.out.print("Enter second input value:");
+		int b = sc.nextInt();
+		System.out.print("Enter third input value:");
+		int c = sc.nextInt();
+		
+		if(a<b && a<c)
+		{
+			System.out.println("The lowest number is: "+a);
+		}
+		if(b<a && b<c)
+		{
+			System.out.println("The lowest number is: "+b);
+		}
+		if(c<b && c<a)
+		{
+			System.out.println("The lowest number is: "+c);
+		}
+		
+
+	}
+
+}
