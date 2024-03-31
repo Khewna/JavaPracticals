d46b09262a0368a72d92877197234bb00b27a733
Khewna <165148322+Khewna@users.noreply.github.com>
2024-03-31 17:13:29 +0530

    Create Practical12.java

---
+++ Practical12.java
@@ -0,0 +1,18 @@
+package Practicals;
+
+import java.util.*;
+
+public class Practical12 {
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		for( int i=1;i<11;i++)
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
