b5bea59ee4e0cdafde796357d7b94f8f3940b98a
Khewna <165148322+Khewna@users.noreply.github.com>
2024-03-31 17:11:54 +0530

    Create Practical10.java

---
+++ Practical10.java
@@ -0,0 +1,25 @@
+package Practicals;
+
+import java.util.*;
+
+public class Practical10 {
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Character:");
+		String a = sc.next();
+		
+		if(a.length() > 1)
+		{
+			System.out.print("Error");
+		}
+		else
+		{
+			System.out.print(a);
+		}
+		
+		
+	}
+
+}
