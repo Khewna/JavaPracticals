2739721917cac46f5d8bfd8ba52330851b6a48e4
Khewna <165148322+Khewna@users.noreply.github.com>
2024-03-31 17:10:00 +0530

    Create Practical6.java

---
+++ Practical6.java
@@ -0,0 +1,17 @@
+package Practicals;
+
+import java.util.*;
+
+public class Practical6 {
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter input value(in km):");
+		double a = sc.nextDouble();
+		
+		double b = a * 0.62;
+		System.out.println("Value(in miles): "+b);
+	}
+
+}
